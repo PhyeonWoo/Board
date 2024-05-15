330f5f854c0dbb390db9c27f4edb05662d4dc079
박현우 <simuoo96@gmail.com>
2024-05-15 12:00:36 +0900

    docs : 리팩토링

--- src/main/java/com/example/Board/controller/BoardController.java
+++ src/main/java/com/example/Board/controller/BoardController.java
@@ -16,7 +16,8 @@ public class BoardController {
     private final BoardService boardService;
 
     // 전체 목록 조회
-    @GetMapping("/api/post")
+    @GetMapping("/api/posts")
+    // GET 방식 "/api/posts" -> getPosts (전체 목록을 가져온다)
     public List<BoardResponseDto> getPosts() {
         return boardService.getPosts();
         // 게시글 내용을 BoardResponseDto에 담아 List형태로 Client에 보낸다
@@ -24,14 +25,20 @@ public class BoardController {
 
     // 게시글 작성
     @PostMapping("/api/post")
+    // POST 방식 "/api/post" -> createPost (게시글을 작성한다)
     public BoardResponseDto createPost(@RequestBody BoardRequestsDto requestsDto) {
         return boardService.createPosts(requestsDto);
+    // 게시글 내용이 담긴 BoardRequestDto를 Client로부터 받는다
+    // 작성된 게시글을 BoardResponseDto에 담아 Client로 보낸다
     }
 
     // 선택한 게시글 조회
     @GetMapping("/api/post/{id}")
+    // GET 방식 "/api/post/{id}" -> getPost (id에 해당하는 글을 조회한다)
     public BoardResponseDto getPosts(@PathVariable Long id) {
         return boardService.getPosts(id);
+    // 선택한 게시물의 id를 Param 형태로 Client로부터 받는다
+    // 조회된 게시글을 BoardResponseDto 에 담아 Client로 보낸다
     }
 
     // 선택한 게시글 수정
