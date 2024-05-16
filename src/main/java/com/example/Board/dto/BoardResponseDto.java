067f5bc807089e56c4eed6f16a7be40fa56ab083
박현우 <simuoo96@gmail.com>
2024-05-16 19:02:44 +0900

    docs : 리팩토링

--- src/main/java/com/example/Board/dto/BoardResponseDto.java
+++ src/main/java/com/example/Board/dto/BoardResponseDto.java
@@ -15,6 +15,10 @@ import java.time.LocalDateTime;
  * 데이터를 담는 용도이기 때문에 필드값,Getter,Setter만 존재하며 다른 메서드는 존재X
  */
 
+
+
+// 게시글을 반환해야 하는 경우 데이터 필드 값을 모아 Dto 구현
+// Board 엔티티를 받아서 객체로 만들기 위해 생성자 추가
 @Getter
 @NoArgsConstructor
 public class BoardResponseDto {
