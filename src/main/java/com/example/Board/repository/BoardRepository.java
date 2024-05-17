0c6c4ea467516aff59b0ba4710cf6f619ad29a18
박현우 <simuoo96@gmail.com>
2024-05-17 22:37:53 +0900

    docs : 리팩토링

--- src/main/java/com/example/Board/repository/BoardRepository.java
+++ src/main/java/com/example/Board/repository/BoardRepository.java
@@ -8,5 +8,8 @@ import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
+
+    // ModifiedAt 내림차순 기준으로 모든 데이터를 가져오기 위해,
+    // Repository에 선언해주면 Service에서 메서드를 사용할 수 있다
     List<Board> findAllByOrderByModifiedAtDesc();
 }
