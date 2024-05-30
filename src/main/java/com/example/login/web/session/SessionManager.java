092b62effeb61a8fe7c26392cbe27237d8c254d6
박현우 <simuoo96@gmail.com>
2024-05-31 01:15:54 +0900

    feat : session 구현

---
+++ src/main/java/com/example/login/web/session/SessionManager.java
@@ -0,0 +1,64 @@
+package com.example.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component // 스프링 빈으로 자동 등록
+public class SessionManager {
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String,Object> sessionStore = new ConcurrentHashMap<>();
+
+    /**
+     * 세션 생성
+     */
+    public void createSession(Object value, HttpServletResponse response) {
+
+        // 세션id를 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId,value);
+
+        // 쿠키 생성
+        Cookie mysessionCookie = new Cookie(SESSION_COOKIE_NAME,sessionId); // 이름은 mysessionId 이고, value는 sessionId이다
+        response.addCookie(mysessionCookie);
+    }
+
+    /**
+     * 세션 조회
+     */
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request,SESSION_COOKIE_NAME);
+        if(sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request,SESSION_COOKIE_NAME);
+        if(sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if(request.getCookies() == null) {
+            return null;
+        }
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+
+}
